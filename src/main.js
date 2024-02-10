acfcb2fbe17fef07088ad7a840e22fa7a0ca1455
Katie27 <wbdl2747@gmail.com>
2024-02-10 22:39:54 +0900

    JS-ArrowUp

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 // header에 페이지 아래로 스크롤시 다크 스타일링 적용
 const header = document.querySelector('.header');
 const headerHeight = header.getBoundingClientRect().height;	// getBoundingClientRect 높이를 반환해주는 함수임
-console.log(headerHeight);
 
 document.addEventListener('scroll', () => {
 	if(window.screenY > headerHeight) {
@@ -17,20 +16,25 @@ document.addEventListener('scroll', () => {
 const home = document.querySelector('.home__container');
 const homeHeigth = home.offsetHeight;
 document.addEventListener('scroll', () => {
-	console.log(1 - window.screenY / homeHeigth);
-	home.style.opacity = 1 - window.scroll / homeHeigth;
-
+	home.style.opacity = 1 - window.scrollY / homeHeigth;
 });
 
-// document.addEventListener('scroll', () => {
-// 	const scrollY = window.scroll.screenY;
-// 	const homeStartY = home.getBoundingClientRect().height;
-// 	const honeEndY = home.getBoundingClientRect().bottom;
-// 	const homeHeigth = honeEndY - homeStartY;
 
-// 	if(scrollY > homeHeigth) {
-// 		home.classList.add('home--fadeout');
-// 	} else {
-// 		home.classList.remove('home--fadeout');
-// 	}
-// });
+// home이 절반 정도 내려가면 Arrow button이 생기고 올라가면 사라지게 하기
+const arrowUp = document.querySelector('.arrow');
+document.addEventListener('scroll', () => {
+
+	// 방법 1
+	// if(window.scrollY > 350) {
+	// 	arrowUp.classList.remove('arrow--visible');
+	// } else {
+	// 	arrowUp.classList.add('arrow--visible');
+	// }
+
+	// 방법 2
+	arrowUp.style.opacity = 2 - homeHeigth  / window.scrollY;
+});
+
+
+// 방법 home높이 기준이니까 homeHeigth이 변수를 가져와서 쓰면 되겠다 싶었지만
+// 나는 윈도우 기준에서 350까지 내려가면 화살표가 지워지게 구현하였다.
