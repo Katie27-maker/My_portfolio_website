5d08428e864921eb844cb870a1915ea232d27cd2
Katie27 <wbdl2747@gmail.com>
2024-03-01 16:52:06 +0900

    html, css - 수정

--- src/slider.js
+++ src/slider.js
@@ -1,14 +1,29 @@
-let image = document.querySelector('.project__img');
-let text = document.querySelector('.project__details');
-let project = document.querySelector('.project');
+let image = document.querySelectorAll('.project__img');
+let details = document.querySelectorAll('.project__details');
+let project = document.querySelectorAll('.project');
 
 
-project.addEventListener("mouseover", function(){
-	image.style.display = "none";
-	text.style.display = "block";
-});
+project.forEach((e, index)=> {
+	e.addEventListener("mouseover", function () {console.log(image[index]);
+		image[index].style.display = "none";
+		details[index].style.display = "block";
+	})
 
-project.addEventListener("mouseout", function(){
-	image.style.display = "block";
-	text.style.display = "none";
-});
+	e.addEventListener("mouseout", function () {
+		image[index].style.display = "block";
+		details[index].style.display = "none";
+	})
+
+	
+})
+
+
+// project.addEventListener("mouseover", function(){
+// 	image.style.display = "none";
+// 	text.style.display = "block";
+// });
+
+// project.addEventListener("mouseout", function(){
+// 	image.style.display = "block";
+// 	details.style.display = "none";
+// });
